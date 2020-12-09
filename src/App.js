f71a99dc5ba9701bce62c4302aa24e886dc4174d
jasurkurbanovinit <jasurkurbanov96@gmail.com>
2020-12-09 12:27:53 +0500

    home screen ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<nav
+				className='navbar is-black'
+				role='navigation'
+				aria-label='main navigation'>
+				<div className='navbar-brand'>
+					<a className='navbar-item' href='https://bulma.io'>
+						<img
+							alt='logo'
+							src='https://bulma.io/images/bulma-logo-white.png'
+							width='112'
+							height='28'
+						/>
+					</a>
+				</div>
+
+				<div id='navbarBasicExample' className='navbar-menu'>
+					<div className='navbar-end'>
+						<div className='navbar-item'>
+							<div className='buttons'>
+								<a href='.' className='button is-primary'>
+									<strong>Sign up</strong>
+								</a>
+								<a href='.' className='button is-light'>
+									Log in
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</nav>
+			<div className='container is-max-desktop mt-3'>
+				<div className='is-flex is-flex-direction-row is-justify-content-space-between'>
+					<button className='button is-primary'>Search</button>
+					<input
+						className='input'
+						type='text'
+						placeholder='enter company name'
+					/>
+				</div>
+				<div class='columns is-mobile mt-3'>
+					<div class='column'>
+						<div className='card'>
+							<div className='card-image'>
+								<figure className='image is-4by3'>
+									<img
+										src='https://bulma.io/images/placeholders/1280x960.png'
+										alt='Placeholder image'
+									/>
+								</figure>
+							</div>
+							<div className='card-content'>
+								<div className='content'>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									Phasellus nec iaculis mauris.
+								</div>
+								<div className='buttons'>
+									<button className='button is-primary'>View</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='column'>
+						<div className='card'>
+							<div className='card-image'>
+								<figure className='image is-4by3'>
+									<img
+										src='https://bulma.io/images/placeholders/1280x960.png'
+										alt='Placeholder image'
+									/>
+								</figure>
+							</div>
+							<div className='card-content'>
+								<div className='content'>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									Phasellus nec iaculis mauris.
+								</div>
+								<div className='buttons'>
+									<button className='button is-primary'>View</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class='column'>
+						<div className='card'>
+							<div className='card-image'>
+								<figure className='image is-4by3'>
+									<img
+										src='https://bulma.io/images/placeholders/1280x960.png'
+										alt='Placeholder image'
+									/>
+								</figure>
+							</div>
+							<div className='card-content'>
+								<div className='content'>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+									Phasellus nec iaculis mauris.
+								</div>
+								<div className='buttons'>
+									<button className='button is-primary'>View</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
